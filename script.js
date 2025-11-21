de32cd9dcecd4c8546747cf9f7ac06c237a74d3b
Vinicius Santos Camelo <vini976964150@gmail.com>
2025-11-21 20:10:36 -0300

    acho que deu bom

--- script.js
+++ script.js
@@ -1,192 +1,205 @@
 function Comofunciona() {
-    var destino = document.getElementById("ComoFunciona");
+  var destino = document.getElementById("ComoFunciona");
 
-    if (destino) {
-        destino.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        });
-    }
+  if (destino) {
+    destino.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }
 }
 function Inovações() {
-    var destino = document.getElementById("inovacoes");
+  var destino = document.getElementById("inovacoes");
 
-    if (destino) {
-        destino.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        });
-    }
+  if (destino) {
+    destino.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }
 }
 function Inovações2() {
-    var destino = document.getElementById("inovacoes2");
+  var destino = document.getElementById("inovacoes2");
 
-    if (destino) {
-        destino.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        });
-    }
+  if (destino) {
+    destino.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }
 }
 function Rastreamento() {
-    var destino = document.getElementById("Rastreamentor");
+  var destino = document.getElementById("Rastreamentor");
 
-    if (destino) {
-        destino.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        });
-    }
+  if (destino) {
+    destino.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }
 }
 function Contato() {
-    var destino = document.getElementById("contato");
+  var destino = document.getElementById("contato");
 
-    if (destino) {
-        destino.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        });
-    }
+  if (destino) {
+    destino.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }
 }
 const pagina = document.getElementById("pagina");
-const botaoTrocarTemaElems = document.querySelectorAll('.trocarTema');
+const botaoTrocarTemaElems = document.querySelectorAll(".trocarTema");
 
 function trocarTema() {
-    const isDark = pagina.classList.contains('tema-dark');
-    if (isDark) {
-        pagina.classList.remove('tema-dark');
-        botaoTrocarTemaElems.forEach(btn => btn.textContent = "Alterar Para Tema Escuro");
-        localStorage.setItem('tema', 'claro');
-    } else {
-        pagina.classList.add("tema-dark");
-        botaoTrocarTemaElems.forEach(btn => btn.textContent = "Alterar Para Tema Claro");
-        localStorage.setItem('tema', 'escuro');
-    }
+  const isDark = pagina.classList.contains("tema-dark");
+  if (isDark) {
+    pagina.classList.remove("tema-dark");
+    botaoTrocarTemaElems.forEach(
+      (btn) => (btn.textContent = "Alterar Para Tema Escuro")
+    );
+    localStorage.setItem("tema", "claro");
+  } else {
+    pagina.classList.add("tema-dark");
+    botaoTrocarTemaElems.forEach(
+      (btn) => (btn.textContent = "Alterar Para Tema Claro")
+    );
+    localStorage.setItem("tema", "escuro");
+  }
 }
 
-botaoTrocarTemaElems.forEach(btn => btn.addEventListener('click', trocarTema));
+botaoTrocarTemaElems.forEach((btn) =>
+  btn.addEventListener("click", trocarTema)
+);
 
 (function carregarTema() {
-    const temaSalvo = localStorage.getItem('tema');
-    if (temaSalvo === 'escuro') {
-        pagina.classList.add('tema-dark');
-        botaoTrocarTemaElems.forEach(btn => btn.textContent = "Alterar Para Tema Claro");
-    } else {
-        pagina.classList.remove('tema-dark');
-        botaoTrocarTemaElems.forEach(btn => btn.textContent = "Alterar Para Tema Escuro");
-    }
+  const temaSalvo = localStorage.getItem("tema");
+  if (temaSalvo === "escuro") {
+    pagina.classList.add("tema-dark");
+    botaoTrocarTemaElems.forEach(
+      (btn) => (btn.textContent = "Alterar Para Tema Claro")
+    );
+  } else {
+    pagina.classList.remove("tema-dark");
+    botaoTrocarTemaElems.forEach(
+      (btn) => (btn.textContent = "Alterar Para Tema Escuro")
+    );
+  }
 })();
 function nao() {
-    const email = document.getElementById('email').value;
-    const senha = document.getElementById('senha').value;
-    const nome = document.getElementById('nome').value;
+  const email = document.getElementById("email").value;
+  const senha = document.getElementById("senha").value;
+  const nome = document.getElementById("nome").value;
 
-    if (!nome) {
-        alert("Sem nome");
-        return
-    } else if (!email) {
-        alert("Sem email")
-        return
-    } else if (!senha) {
-        alert("Sem senha")
-        return
-    } else {
-        localStorage.setItem("nome", nome);
-        localStorage.setItem("email", email);
-        localStorage.setItem("senha", senha);
-        window.location.href = "Tela de Login.html"
-    }
+  if (!nome) {
+    alert("Sem nome");
+    return;
+  } else if (!email) {
+    alert("Sem email");
+    return;
+  } else if (!senha) {
+    alert("Sem senha");
+    return;
+  } else {
+    localStorage.setItem("nome", nome);
+    localStorage.setItem("email", email);
+    localStorage.setItem("senha", senha);
+    window.location.href = "Tela de Login.html";
+  }
 }
 function sim() {
-    const email = document.getElementById('email').value;
-    const senha = document.getElementById('senha').value;
+  const email = document.getElementById("email").value;
+  const senha = document.getElementById("senha").value;
 
-
-    if (!email) {
-        alert("Sem email")
-        return
-    } else if (!senha) {
-        alert("Sem senha")
-        return
-    } else {
-
-        localStorage.setItem("email", email);
-        localStorage.setItem("senha", senha);
-        window.location.href = "index.html"
-    }
-
+  if (!email) {
+    alert("Sem email");
+    return;
+  } else if (!senha) {
+    alert("Sem senha");
+    return;
+  } else {
+    localStorage.setItem("email", email);
+    localStorage.setItem("senha", senha);
+    window.location.href = "index.html";
+  }
 }
 const SPINNER_HTML = '<div class="spinner"></div>';
 const SPINNER_HTML2 = '<div class="spinner2"></div>';
 
 function rastrear() {
-    const botao = document.getElementById('botaoRastrear');
-    const main = document.getElementById('rastreamentos');
-    const inputCodigo = document.getElementById('codigo');
-    const cg = inputCodigo ? inputCodigo.value : '';
+  const botao = document.getElementById("botaoRastrear");
+  const main = document.querySelector(".rastreamento");
+  const inputCodigo = document.getElementById("codigo");
+  const cg = inputCodigo ? inputCodigo.value : "";
 
-    if (!cg) {
-        alert("Codigo não encontrado ou não colocado!");
-        return;
-    }
+  if (!cg) {
+    alert("Codigo não encontrado ou não colocado!");
+    return;
+  }
 
-    const textoOriginal = botao.textContent;
+  const textoOriginal = botao.textContent;
 
-    botao.innerHTML = SPINNER_HTML;
-    botao.classList.add('loading-button', 'disabled-button');
-    botao.disabled = true;
+  botao.innerHTML = SPINNER_HTML;
+  botao.classList.add("loading-button", "disabled-button");
+  botao.disabled = true;
 
+  setTimeout(() => {
+    main.innerHTML = `
+    <h2 class="ha">Rastreamento ao Vivo</h2>
+    <p>Status do Pedido: Em transporte 🚚</p>
 
+    <div class="exemplo">
 
+        <div class="map-container">
 
-    setTimeout(() => {
+            <div class="map-zoom">
+                <svg viewBox="0 0 600 300" class="map-svg">
 
+                
+                  <polyline
+                        class="neon-line"
+                        points="70,120 150,160 220,180 300,200 380,260"
+                    />
 
-        main.innerHTML = `
+                
+                    <circle cx="70" cy="120" r="8" class="dot"/>
+                    <circle cx="150" cy="160" r="8" class="dot"/>
+                    <circle cx="220" cy="180" r="8" class="dot"/>
+                    <circle cx="300" cy="200" r="8" class="dot"/>
 
-          <div class="come">
-            <img src="img/logo foguete.png" alt="foguete">
-            <h1> Rastreamento em Tempo Real</h1>
-          </div>
-          <p>Acompanhe seu pedido se movendo pelo mundo em tempo real com nossa tecnologia de Ponta</p>
-          <div class="inp">
-            <input type="text" class="inpv" id="codigo"
-              placeholder="Digite o codigo o codigo de Rastreamento Ex:T3R7G9">
-            <button id="botaoRastrear" onclick="rastrear()">Rastreamento ao Vivo</button>
-          </div>
-          <p>Experimente nosso sistema de rastreamento ao vivo! Digite qualquer código para ver a mágica acontecer.</p>
-          <div class="animação">
-            <div class="cansaso">
-              <h1>Mapa de Rastreamento Interativo</h1>
+                
+                    <circle cx="380" cy="260" r="10" class="dot final-dot"/>
+                    <circle cx="380" cy="260" r="30" class="energy-ring"/>
+
+                  <image
+                        href="img/caminhao-de-carga.png"
+                        x="70"
+                        y="110"
+                        width="28"
+                        height="28"
+                        class="truck"
+                    />
+                </svg>
             </div>
-            <div class="exemplo"></div>
-            <div class="alinha">
-              <div class="cafe">
-                <h1 class="h1">100%</h1>
-                <p>Completo</p>
-              </div>
-              <div class="cafe">
-                <h1 class="h2">1d</h1>
-                <p>Restantes</p>
-              </div>
-              <div class="cafe">
-                <h1 class="h3">12450 km</h1>
-                <p>Percorridos</p>
-              </div>
-              <div class="cafe">
-                <h1 class="h4">✓</h1>
-                <p>Status</p>
-              </div>
-            </div>
-          </div>
 
-        `;
+            
+            <div class="city" style="top: 28%; left: 6%;">São Paulo</div>
+            <div class="city" style="top: 44%; left: 20%;">Curitiba</div>
+            <div class="city" style="top: 50%; left: 32%;">Joinville</div>
+            <div class="city" style="top: 58%; left: 46%;">Florianópolis</div>
+            <div class="city" style="top: 78%; left: 60%;">Porto Alegre</div>
+        </div>
 
-    }, 3000);
+    </div>
+
+    <h3 class="he">Endereço: Rua teste, nº 1234</h3>
+    <h3 class="he">Prazo estimado: 3 dias úteis</h3>
+    `;
+  }, 2000);
 }
 function cadastrar() {
-    const main = document.getElementById('conteudo');
-    main.innerHTML = `
+  const main = document.getElementById("conteudo");
+  main.innerHTML = `
         <div class="Cadastrodivconteiner">
             <img src="img/LogoTechShip.png" alt="logo">
             <p class="Titulotechship">TECH SHIP</p>
@@ -206,8 +219,8 @@ function cadastrar() {
     `;
 }
 function login() {
-    const main = document.getElementById('conteudo');
-    main.innerHTML = `
+  const main = document.getElementById("conteudo");
+  main.innerHTML = `
         <div class="Cadastrodivconteiner">
             <img src="img/LogoTechShip.png" alt="logo">
             <p class="Titulotechship">TECH SHIP</p>
@@ -225,30 +238,25 @@ function login() {
     `;
 }
 function calcular() {
-    const main = document.getElementById('calcular4');
-    const codigo = document.getElementById('codigo2');
-    const botao = document.getElementById('botaoc');
-    const cg = codigo ? codigo.value : '';
+  const main = document.getElementById("calcular4");
+  const codigo = document.getElementById("codigo2");
+  const botao = document.getElementById("botaoc");
+  const cg = codigo ? codigo.value : "";
 
+  if (!cg) {
+    alert("Codigo não encontrado ou não colocado!");
+    return;
+  }
 
+  const textoOriginal = botao.textContent;
 
-    if (!cg) {
-        alert("Codigo não encontrado ou não colocado!");
-        return;
-    }
+  botao.innerHTML = SPINNER_HTML2;
+  botao.classList.add("loading-button2", "disabled-button");
+  botao.disabled = true;
 
-    const textoOriginal = botao.textContent;
-
-    botao.innerHTML = SPINNER_HTML2;
-    botao.classList.add('loading-button2', 'disabled-button');
-    botao.disabled = true;
-
-
-    setTimeout(() => {
-
-
-        main.classList.add("headerCalculadoraextendida");
-        main.innerHTML = `
+  setTimeout(() => {
+    main.classList.add("headerCalculadoraextendida");
+    main.innerHTML = `
         <div class="headerCalculadora">
                 <h2>Calculadora de IA</h2>
                 <p class="p0">Cálculo instantâneo com análise preditiva</p>
@@ -283,114 +291,98 @@ function calcular() {
                     </div>
                 </div>
                 </div>
-            `
-
-
-
-
-    }, 3000);
-
+            `;
+  }, 3000);
 }
 function abrirMenu() {
-    const a2 = document.getElementById('a2');
-    const menui = document.getElementById('menui');
-    const header1 = document.getElementById("header1");
+  const a2 = document.getElementById("a2");
+  const menui = document.getElementById("menui");
+  const header1 = document.getElementById("header1");
 
-    menui.classList.add("sumir2");
-    a2.classList.add("a22");
-    header1.classList.add("umembaixo");
+  menui.classList.add("sumir2");
+  a2.classList.add("a22");
+  header1.classList.add("umembaixo");
 }
 function fecharMenu() {
-    const a2 = document.getElementById('a2');
-    const menui = document.getElementById('menui');
-    const header1 = document.getElementById("header1");
+  const a2 = document.getElementById("a2");
+  const menui = document.getElementById("menui");
+  const header1 = document.getElementById("header1");
 
-    menui.classList.remove("sumir2");
-    a2.classList.remove("a22");
-    header1.classList.remove("umembaixo");
+  menui.classList.remove("sumir2");
+  a2.classList.remove("a22");
+  header1.classList.remove("umembaixo");
 }
 function levarCadastro() {
-    window.location.href = "Tela de Cadastro.html";
+  window.location.href = "Tela de Cadastro.html";
 }
 function comecarImp() {
-    window.location.href = "imp.html";
+  window.location.href = "imp.html";
 }
 function comoimp() {
+  const paginaDestino = "index.html?scroll=ComoFunciona";
 
-    const paginaDestino = "index.html?scroll=ComoFunciona";
-    
-
-    window.location.href = paginaDestino;
+  window.location.href = paginaDestino;
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    const urlParams = new URLSearchParams(window.location.search);
-    
-    const elementoId = urlParams.get('scroll'); 
+document.addEventListener("DOMContentLoaded", function () {
+  const urlParams = new URLSearchParams(window.location.search);
 
-    if (elementoId) {
+  const elementoId = urlParams.get("scroll");
 
-        const destino = document.getElementById(elementoId);
+  if (elementoId) {
+    const destino = document.getElementById(elementoId);
 
-        if (destino) {
-
-            destino.scrollIntoView({
-                behavior: 'smooth',
-                block: 'start'
-            });
-        }
+    if (destino) {
+      destino.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
     }
+  }
 });
 function rastreimp() {
+  const paginaDestino = "index.html?scroll=Rastreamentor";
 
-    const paginaDestino = "index.html?scroll=Rastreamentor";
-    
-
-    window.location.href = paginaDestino;
+  window.location.href = paginaDestino;
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    const urlParams = new URLSearchParams(window.location.search);
-    
-    const elementoId = urlParams.get('scroll'); 
+document.addEventListener("DOMContentLoaded", function () {
+  const urlParams = new URLSearchParams(window.location.search);
 
-    if (elementoId) {
+  const elementoId = urlParams.get("scroll");
 
-        const destino = document.getElementById(elementoId);
+  if (elementoId) {
+    const destino = document.getElementById(elementoId);
 
-        if (destino) {
-
-            destino.scrollIntoView({
-                behavior: 'smooth',
-                block: 'start'
-            });
-        }
+    if (destino) {
+      destino.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
     }
+  }
 });
 
 function contatoimp() {
+  const paginaDestino = "index.html?scroll=contato";
 
-    const paginaDestino = "index.html?scroll=contato";
-    
-
-    window.location.href = paginaDestino;
+  window.location.href = paginaDestino;
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    const urlParams = new URLSearchParams(window.location.search);
-    
-    const elementoId = urlParams.get('scroll'); 
+document.addEventListener("DOMContentLoaded", function () {
+  const urlParams = new URLSearchParams(window.location.search);
 
-    if (elementoId) {
+  const elementoId = urlParams.get("scroll");
 
-        const destino = document.getElementById(elementoId);
+  if (elementoId) {
+    const destino = document.getElementById(elementoId);
 
-        if (destino) {
-
-            destino.scrollIntoView({
-                behavior: 'smooth',
-                block: 'start'
-            });
-        }
+    if (destino) {
+      destino.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
     }
-});contato
+  }
+});
+contato;
